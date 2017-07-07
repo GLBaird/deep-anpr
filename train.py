5f20b58e475786b3de5dbdc92c595ad1bd7d985a
Leon Baird <leon.baird@qumo.do>
2017-07-07 13:42:33 +0100

    Converted for python3 and OpenCV3 with Tensorflow 1.2

--- train.py
+++ train.py
@@ -126,10 +126,8 @@ def get_loss(y, y_):
     # Calculate the loss from digits being incorrect.  Don't count loss from
     # digits that are in non-present plates.
     digits_loss = tf.nn.softmax_cross_entropy_with_logits(
-                                          tf.reshape(y[:, 1:],
-                                                     [-1, len(common.CHARS)]),
-                                          tf.reshape(y_[:, 1:],
-                                                     [-1, len(common.CHARS)]))
+        logits=tf.reshape(y[:, 1:], [-1, len(common.CHARS)]),
+        labels=tf.reshape(y_[:, 1:], [-1, len(common.CHARS)]))
     digits_loss = tf.reshape(digits_loss, [-1, 7])
     digits_loss = tf.reduce_sum(digits_loss, 1)
     digits_loss *= (y_[:, 0] != 0)
@@ -137,7 +135,8 @@ def get_loss(y, y_):
 
     # Calculate the loss from presence indicator being wrong.
     presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(
-                                                          y[:, :1], y_[:, :1])
+        logits=y[:, :1],
+        labels=y_[:, :1])
     presence_loss = 7 * tf.reduce_sum(presence_loss)
 
     return digits_loss, presence_loss, digits_loss + presence_loss
@@ -149,7 +148,7 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
 
     The function operates interactively: Progress is reported on stdout, and
     training ceases upon `KeyboardInterrupt` at which point the learned weights
-    are saved to `weights.npz`, and also returned.
+    are saved to `weights_done.npz`, and also returned.
 
     :param learn_rate:
         Learning rate to use.
@@ -202,11 +201,11 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                                               r[3] < 0.5)))
         r_short = (r[0][:190], r[1][:190], r[2][:190], r[3][:190])
         for b, c, pb, pc in zip(*r_short):
-            print "{} {} <-> {} {}".format(vec_to_plate(c), pc,
-                                           vec_to_plate(b), float(pb))
+            print("{} {} <-> {} {}".format(vec_to_plate(c), pc,
+                                           vec_to_plate(b), float(pb)))
         num_p_correct = numpy.sum(r[2] == r[3])
 
-        print ("B{:3d} {:2.02f}% {:02.02f}% loss: {} "
+        print(("B{:3d} {:2.02f}% {:02.02f}% loss: {} "
                "(digits: {}, presence: {}) |{}|").format(
             batch_idx,
             100. * num_correct / (len(r[0])),
@@ -215,7 +214,7 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
             r[4],
             r[5],
             "".join("X "[numpy.array_equal(b, c) or (not pb and not pc)]
-                                           for b, c, pb, pc in zip(*r_short)))
+                                           for b, c, pb, pc in zip(*r_short))))
 
     def do_batch():
         sess.run(train_step,
@@ -240,15 +239,15 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                 if batch_idx % report_steps == 0:
                     batch_time = time.time()
                     if last_batch_idx != batch_idx:
-                        print "time for 60 batches {}".format(
+                        print("time for 60 batches {}".format(
                             60 * (last_batch_time - batch_time) /
-                                            (last_batch_idx - batch_idx))
+                                            (last_batch_idx - batch_idx)))
                         last_batch_idx = batch_idx
                         last_batch_time = batch_time
 
         except KeyboardInterrupt:
             last_weights = [p.eval() for p in params]
-            numpy.savez("weights.npz", *last_weights)
+            numpy.savez("weights_done.npz", *last_weights)
             return last_weights
 
 
